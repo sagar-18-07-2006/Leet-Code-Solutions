9b54fa406495e0f15b72c44be003b19f35a55e1a
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-22 19:10:41 +0530

    done with brute force 100% runtime

--- PLus_One_66.cpp
+++ PLus_One_66.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& v) {
-        int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
-                v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
-            }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0;i<n;i++)
+        {
+            nums1.pop_back();
         }
-        return v;
+        for(int val : nums2){
+            nums1.push_back(val);
+
+    }
+    
+    sort(nums1.begin(),nums1.end());
     }
 };
 
 
 int main (){
-    vector<int> nums ;
-    cout<<"enter size of vectors"<<endl;
+    vector<int> nums1,nums2 ;
+    cout<<"enter size of vector 1"<<endl;
+    int m;
+    cin>>m;
+    for(int i=0; i<m;i++){
+        cout<<"enter "<<i<<" element of vector"<<endl;
+      int j;
+      cin>>j;
+      nums1.push_back(j);
+    }
+    cout<<"enter size of vector 2"<<endl;
     int n;
     cin>>n;
     for(int i=0; i<n;i++){
         cout<<"enter "<<i<<" element of vector"<<endl;
       int j;
       cin>>j;
-      nums.push_back(j);
+      nums2.push_back(j);
     }
+    
     Solution a;
- a.plusOne(nums);
- for (int val : nums){
+ a.merge(nums1,m,nums2,n);
+ for (int val : nums1){
     cout<<val;
  }
 }
