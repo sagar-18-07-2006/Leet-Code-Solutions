4aecd4db4298d065fb5eab2ed19d6f7ec8cf279c
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-22 18:53:22 +0530

    plus one problem teach me that we can add zero at back to increase length of nums

---
+++ PLus_One_66.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) {
+        int n = v.size();
+        for(int i = n-1; i >= 0; i--){
+            if(i == n-1)
+                v[i]++;
+            if(v[i] == 10){
+                v[i] = 0;
+                if(i != 0){
+                    v[i-1]++;
+                }
+                else{
+                    v.push_back(0);
+                    v[i] = 1;
+                }
+            }
+        }
+        return v;
+    }
+};
+
+
+int main (){
+    vector<int> nums ;
+    cout<<"enter size of vectors"<<endl;
+    int n;
+    cin>>n;
+    for(int i=0; i<n;i++){
+        cout<<"enter "<<i<<" element of vector"<<endl;
+      int j;
+      cin>>j;
+      nums.push_back(j);
+    }
+    Solution a;
+ a.plusOne(nums);
+ for (int val : nums){
+    cout<<val;
+ }
+}
