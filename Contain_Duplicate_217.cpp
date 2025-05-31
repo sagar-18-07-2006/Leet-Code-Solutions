f6c5f34a841b9e4f85b8f68273329a63c8566666
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-31 14:24:06 +0530

    done contain duplicate

---
+++ Contain_Duplicate_217.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+};
+int main (){
+    vector<int> nums ;
+    cout<<"enter size of vectors"<<endl;
+    int n;
+    cin>>n;
+     for(int i=0; i<n;i++){
+        cout<<"enter "<<i<<" element of vector"<<endl;
+      int j;
+      cin>>j;
+      nums.push_back(j);
+    }
+  Solution a;
+  bool v= a.containsDuplicate(nums);
+
+  
+  
+}
